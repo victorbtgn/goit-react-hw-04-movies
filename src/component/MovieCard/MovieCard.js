c532f918381d1a77f03866f30ead10860ba6ebe2
Victor Batigin <bixs85@gmail.com>
2020-07-05 16:50:37 +0300

    add style for movie card

--- src/component/MovieCard/MovieCard.js
+++ src/component/MovieCard/MovieCard.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
+import styles from './MovieCard.module.css';
 
 const MovieCard = ({ films, location }) => (
   <>
     {films.map(({ id, title, poster_path }) => (
-      <NavLink
-        key={id}
-        to={{
-          pathname: `/movies/${id}`,
-          state: { from: location },
-        }}
-      >
-        <li>
-          <p>{title}</p>
-          <img
-            src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
-            alt={title}
-          />
-        </li>
-      </NavLink>
+      <li key={id} className={styles.MovieCard}>
+        {poster_path && (
+          <NavLink
+            to={{
+              pathname: `/movies/${id}`,
+              state: { from: location },
+            }}
+            className={styles.link}
+          >
+            <p>{title}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
+              alt={title}
+              className={styles.link}
+            />
+          </NavLink>
+        )}
+      </li>
     ))}
   </>
 );
